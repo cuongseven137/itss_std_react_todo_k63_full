9fd640ed3213551dfb7f7ec3a7116372ad123d5e
Đinh Quốc Cường <quoccuong137@gmail.com>
2022-06-30 21:52:26 +0700

    update

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,20 +1,107 @@
-// Import the functions you need from the SDKs you need
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBLrDEiSak8L7vLdPILQVTCsgiIyWKbSK4",
-  authDomain: "itss-std-react-todo-k63-47558.firebaseapp.com",
-  projectId: "itss-std-react-todo-k63-47558",
-  storageBucket: "itss-std-react-todo-k63-47558.appspot.com",
-  messagingSenderId: "961395719793",
-  appId: "1:961395719793:web:5101896df604fc1cca65d0",
-  measurementId: "G-PKNKP9XFQE"
+const firebaseConfig = { 
+  apiKey : "AIzaSyBLrDEiSak8L7vLdPILQVTCsgiIyWKbSK4" , 
+  authDomain : "itss-std-react-todo-k63-47558.firebaseapp.com" , 
+  projectId : "itss-std-react-todo-k63-47558" , 
+  storageBucket : "itss-std-react-todo-k63-47558.appspot.com" , 
+  messagingSenderId : "961395719793" , 
+  appId : "1:961395719793:web:0d1a7ef1f38ab457ca65d0" , 
 };
-// Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+export const auth = firebase.auth();
+export default firebase;
+
+export const getFirebaseItems = async () => {
+  try {
+    const snapshot = await db
+      .collection("todos")
+      .get();
+    const items = snapshot.docs.map(
+      (doc) => ({ ...doc.data(), id: doc.id })
+    );
+    return items;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
+
+export const addFirebaseItem = async (item) => {
+  try {
+    const todoRef = db.collection("todos");
+    await todoRef.add(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const updateFirebaseItem = async (item, id) => {
+  try {
+    const todoRef = db.collection("todos").doc(id);
+    await todoRef.update(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const clearFirebaseItem = async (item) => {
+  const todoRef = db.collection("todos").doc(item.id);
+  await todoRef.delete().then(function () {
+  }).catch(function (err) {
+    console.log(err);
+  });
+};
+
+export const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: "/",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+}
+
+export const storeUserInfo = async (user) => {
+  const { uid } = user;
+  const userDoc = await db.collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await db.collection("users").doc(uid).set({ name: user.displayName });
+    return {
+      name: user.displayName,
+      id: uid,
+    };
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+    };
+  }
+}
+
+export const updateUser = async (user, image) => {
+  try {
+    const userDoc = await firebase.firestore().collection("users").doc(user.id).get();
+    if (userDoc.exists) {
+      await firebase.firestore().collection("users").doc(user.id).update({ ...userDoc.data(), image: image });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const uploadImage = async (image) => {
+  const ref = firebase.storage().ref().child(`/images/${image.name}`);
+  let downloadUrl = "";
+  try {
+    await ref.put(image);
+    downloadUrl = await ref.getDownloadURL();
+  } catch (err) {
+    console.log(err);
+  }
+  return downloadUrl;
+};
